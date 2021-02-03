1b793af8b6751def3923d09a518cfdb90fd02128
Denisnjeru <njerudenis0@gmail.com>
2021-02-03 14:44:51 +0300

    changed the package name to ehd

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Njeru", # Replace with your own username
+    name="ehd", # Replace with your own username
     version="0.0.1",
     author="Njeru",
     author_email="njerudenis0@gmail.com",
     description="To use for my projects",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Denisnjeru/EHD.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
